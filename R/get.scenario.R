f186828ebc189049ca09123b2c3c54968cbeeb1a
Sean McGrath <smcgrath@hsph.harvard.edu>
2019-02-12 11:40:46 -0500

    Removed export of get.scenario() function

--- R/get.scenario.R
+++ R/get.scenario.R
@@ -1,23 +1,3 @@
-#' Classify scenario based on summary measures
-#'
-#' The function indicates whether S1, S2, S3, or S4 summary measures are provided.  See documentation of \code{\link{qe}} for definitions of S1, S2, S3, and S4 summary measures. Note that the sample size is not included in this check.
-#'
-#' @param min.val numeric value giving the sample minimum.
-#' @param q1.val numeric value giving the sample first quartile.
-#' @param med.val numeric value giving the sample median.
-#' @param q3.val numeric value giving the sample third quartile.
-#' @param max.val numeric value giving the sample maximum.
-#' @param mean.val numeric value giving the sample mean.
-#' @param sd.val numeric value giving the sample standard deviation.
-#'
-#' @return A character string of one of the following: \code{"S1"}, \code{"S2"}, \code{"S3"}, or \code{"S4"}.
-#'
-#' @examples
-#' scenario <- get.scenario(min.val=1, med.val=5, max.val=20)
-
-#' @export
-
-
 get.scenario <- function(min.val, q1.val, med.val, q3.val, max.val, mean.val,
                          sd.val) {
   if (!(missing(min.val) | missing(q1.val) | missing(med.val) | missing(q3.val)
